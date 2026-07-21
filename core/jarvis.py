67e914823444ddca761999e20133f1cb9fc525b3
symamadou19888-hub <symamadou19888@gmail.com>
2026-07-21 23:49:25 +0000

    Connexion du système de logs au moteur Jarvis

--- core/jarvis.py
+++ core/jarvis.py
@@ -5,6 +5,7 @@ from command_manager import CommandManager
 from memory_manager import MemoryManager
 from skill_manager import SkillManager
 from brain_manager import BrainManager
+from logger import Logger
 
 
 class Jarvis:
@@ -15,6 +16,7 @@ class Jarvis:
         self.command_manager = CommandManager()
         self.skill_manager = SkillManager()
         self.brain_manager = BrainManager()
+        self.logger = Logger()
 
         self.nom = self.config["nom"]
         self.version = self.config["version"]
@@ -26,6 +28,8 @@ class Jarvis:
             return json.load(fichier)
 
     def start(self):
+        self.logger.enregistrer("Démarrage de Jarvis")
+
         print(f"{self.nom} version {self.version} démarré.")
         print("Mémoire chargée.")
         self.agent_manager.afficher_agents()
@@ -37,11 +41,14 @@ class Jarvis:
             commande = input("Vous : ")
 
             if commande.lower() in ["quitter", "exit", "stop"]:
+                self.logger.enregistrer("Arrêt de Jarvis")
                 print("Jarvis arrêté.")
                 break
 
             self.traiter_commande(commande)
 
     def traiter_commande(self, commande):
+        self.logger.enregistrer(f"Commande reçue : {commande}")
+
         resultat = self.command_manager.traiter(commande)
         print(resultat)
