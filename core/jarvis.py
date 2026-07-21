22db2520130384d327c1e09daf5d4a81225e3474
symamadou19888-hub <symamadou19888@gmail.com>
2026-07-21 23:33:36 +0000

    Connexion du gestionnaire de compétences au moteur Jarvis

--- core/jarvis.py
+++ core/jarvis.py
@@ -3,6 +3,7 @@ import os
 from agent_manager import AgentManager
 from command_manager import CommandManager
 from memory_manager import MemoryManager
+from skill_manager import SkillManager
 
 
 class Jarvis:
@@ -11,6 +12,7 @@ class Jarvis:
         self.memory_manager = MemoryManager()
         self.agent_manager = AgentManager()
         self.command_manager = CommandManager()
+        self.skill_manager = SkillManager()
 
         self.nom = self.config["nom"]
         self.version = self.config["version"]
@@ -25,6 +27,7 @@ class Jarvis:
         print(f"{self.nom} version {self.version} démarré.")
         print("Mémoire chargée.")
         self.agent_manager.afficher_agents()
+        self.skill_manager.afficher_skills()
         print("Système prêt.")
 
         while True:
